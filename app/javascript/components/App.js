a3a664da5697279774358873fbd544990bb36106
nobuhiko shimaya <shimayanobuhiko@gmail.com>
2021-10-14 00:10:46 +0900

    components作成

---
+++ app/javascript/components/App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+import styled from 'styled-components'
+import AddTodo from './AddTodo'
+import TodoList from './TodoList'
+import EditTodo from './EditTodo'
+import './App.css'
+
+const Nabver = styled.nav`
+    background: #dbfffe;
+    min-height: 8vh;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+`
+
+
+
+
+    case value:
+        
+        break;
+
+    default:
+        break;
+}}
+
+function app() {
+  return <div>App</div>;
+}
+
+export default app;
